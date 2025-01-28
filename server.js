80b38e0eb7522955004cf74d008318f1fb1a31aa
dhvanil1403 <pd14030304@gmail.com>
2025-01-28 17:22:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const session = require("express-session");
 const app = express();
 const authRoutes = require("./routes/authRoutes");
 const acquisitionRouter = require('./routes/acquisitionRouter');
+
+//sociey router reporting
+const societyRouter = require('./routes/societyRouter');
 const fileUpload = require('express-fileupload');
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +23,9 @@ app.use(
 const inquiryRoutes = require("./routes/inquiryRoutes");
 app.use("/api", inquiryRoutes);
 app.use('/api', acquisitionRouter);
+
+app.use('/api', societyRouter)
+
 app.use(fileUpload());
 // Render Dashboard
 app.get("/dashboard", (req, res) => {
@@ -28,7 +34,7 @@ app.get("/dashboard", (req, res) => {
   }
 
   res.send(
-    `<h1>Welcome ${req.session.user.name}!</h1><a href="/logout">Logout</a>`                                        
+    `<h1>Welcome ${req.session.user.name}!</h1><a href="/logout">Logout</a>`                                         
   );
 });                                                                               
 
@@ -36,4 +42,6 @@ app.get("/dashboard", (req, res) => {
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-});
+});                    
+
+
