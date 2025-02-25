c7415651f0d223e0b57a25058532538ff0bb88ad
dhvanil1403 <pd14030304@gmail.com>
2025-02-25 11:03:49 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -300,7 +300,7 @@ router.post('/employee-location',verifyToken, async (req, res) => {
 async function autoPunchOutTask() {
     try {
         const date = moment().tz(TIMEZONE).format("YYYY-MM-DD");
-        const punchOutTime = moment().tz(TIMEZONE).set({ hour: 21, minute: 0, second: 0 }).format("YYYY-MM-DD HH:mm:ss"); // 9:00 PM
+        const punchOutTime = moment().tz(TIMEZONE).set({ hour: 11, minute: 10, second: 0 }).format("YYYY-MM-DD HH:mm:ss"); // 9:00 PM
 
         const result = await pool.query(
             `SELECT id FROM attendance WHERE punch_out_time IS NULL AND date = $1`,
