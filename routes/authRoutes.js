9e95ad48d72392d66a19db21bfc7195b051a50d0
aekads <aekads.in@gmail.com>
2025-04-02 04:53:38 +0000

    Updated all modified files with the latest changes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -699,7 +699,7 @@ ORDER BY e.emp_id;
         // Email options
         const mailOptions = {
             from: "your-email@gmail.com", // Replace with your email
-            to: "hp9537213@gmail.com, shaikhanish1992@gmail.com, sahaskumbhani221@gmail.com, aravind@aekads.com",
+            to: "hp9537213@gmail.com, shaikhanish1992@gmail.com, sahaskumbhani221@gmail",
         
          
             
