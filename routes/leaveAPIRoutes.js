c44e4f070a82764ae346532e5b0840ad15a7db15
dhvanil1403 <pd14030304@gmail.com>
2025-07-24 12:10:04 +0530

    Create leaveAPIRoutes.js

---
+++ routes/leaveAPIRoutes.js
@@ -0,0 +1,15 @@
+// leaveRoutes.js
+const express = require('express');
+const router = express.Router();
+const leaveController = require('./leaveController');
+
+// POST - Apply for leave
+router.post('/leave/apply', leaveController.applyLeave);
+
+// GET - List all leaves
+router.get('/leave/list', leaveController.getLeaves);
+
+// POST - Cancel leave
+router.post('/leave/cancel', leaveController.cancelLeave);
+
+module.exports = router;
