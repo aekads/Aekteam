ded936f1a11498847bcd2731757c6f00b3da37ba
dhvanil1403 <pd14030304@gmail.com>
2025-08-19 11:32:25 +0530

    Update leaveRoutes.js

--- routes/leaveRoutes.js
+++ routes/leaveRoutes.js
@@ -4,21 +4,22 @@ const leaveController = require("../controllers/leaveController");
 const { verifyToken,requireHR } = require("../config/middleware");
 
 // ✅ Employee Applies for Leave
-router.post("/leave/apply", leaveController.applyLeave);
+router.post("/leave/apply",verifyToken, leaveController.applyLeave);
 
 // ✅ Employee Views Their Leave List
 router.get("/leave/list",verifyToken, leaveController.getLeaves);
 
 
-router.get('/cc', leaveController.getEmployeeList);
+router.get('/cc',verifyToken, leaveController.getEmployeeList);
 
 
-
+// ✅ Get Upcoming Birthdays
+router.get("/birthdays", verifyToken, leaveController.getUpcomingBirthdays);
 
 
 router.get("/leave", verifyToken, leaveController.renderLeavePage);
 
-router.post("/leave/cancel", leaveController.cancelLeave);
+router.post("/leave/cancel",verifyToken, leaveController.cancelLeave);
 
 
 module.exports = router;  
