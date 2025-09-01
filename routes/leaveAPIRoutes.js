3c0a2f7b96286f599b67c78dd5507481cd80dabd
dhvanil1403 <pd14030304@gmail.com>
2025-09-01 14:33:54 +0530

    Update leaveAPIRoutes.js

--- routes/leaveAPIRoutes.js
+++ routes/leaveAPIRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const leaveController = require('../controllers/leaveAPIController'); // ✅ correct path
-
+const employeeController = require('../controllers/employeeController');
 router.post('/leave/apply', leaveController.applyLeave);
 router.get('/leave/list', leaveController.getLeaves);
 router.post('/leave/cancel', leaveController.cancelLeave);
@@ -9,6 +9,8 @@ router.post('/leave/cancel', leaveController.cancelLeave);
 // Get regularization info
 router.post('/regularization/list', leaveController.getRegularizationData);
 
+
+router.post('/punch-status', leaveController.getPunchStatus );
 
 
 // Submit a permission request
