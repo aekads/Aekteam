f2015539f862f857292affa1de28ef023bd53e93
dhvanil1403 <pd14030304@gmail.com>
2025-09-03 14:24:04 +0530

    Update leaveAPIRoutes.js

--- routes/leaveAPIRoutes.js
+++ routes/leaveAPIRoutes.js
@@ -1,21 +1,26 @@
-const express = require('express');
-const router = express.Router();
-const leaveController = require('../controllers/leaveAPIController'); // ✅ correct path
-const employeeController = require('../controllers/employeeController');
-router.post('/leave/apply', leaveController.applyLeave);
-router.get('/leave/list', leaveController.getLeaves);
-router.post('/leave/cancel', leaveController.cancelLeave);
+    const express = require('express');
+    const router = express.Router();
+    const leaveController = require('../controllers/leaveAPIController'); // ✅ correct path
+    const employeeController = require('../controllers/employeeController');
+    router.post('/leave/apply', leaveController.applyLeave);
+    router.get('/leave/list', leaveController.getLeaves);
+    router.post('/leave/cancel', leaveController.cancelLeave);
 
-// Get regularization info
-router.post('/regularization/list', leaveController.getRegularizationData);
-
-
-router.post('/punch-status', leaveController.getPunchStatus );
-router.post('/attendance', leaveController.getAttendanceByDate);
+    // Get regularization info
+    router.post('/regularization/list', leaveController.getRegularizationData);
 
 
-// Submit a permission request
-router.post('/regularization/apply', leaveController.applyPermission);
+    router.post('/punch-status', leaveController.getPunchStatus );
+    router.post('/attendance', leaveController.getAttendanceByDate);
 
 
-module.exports = router;
+    // Submit a permission request
+    router.post('/regularization/apply', leaveController.applyPermission);
+
+// Employee Profile API
+// Get Employee Profile by emp_id (POST)
+router.post('/profile-emp', leaveController.getProfileAPI);
+
+router.post('/profile/update', leaveController.updateProfileAPI);
+
+    module.exports = router;
