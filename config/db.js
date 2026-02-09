8832c73a136f33ce1e003d6b1caa0c6bf3a08a4d
aekads <aekads.in@gmail.com>
2026-02-09 13:02:08 +0530

    d
    dd

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: "u3m7grklvtlo6",
-    host: "35.209.89.182",
+    host: "35.208.232.68",
     database: "dbzvtfeophlfnr",
     password: "AekAds@24",
     port: 5432,
